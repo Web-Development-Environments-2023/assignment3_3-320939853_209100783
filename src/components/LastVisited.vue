d9cef51318e384a7b406f74f432de8054ba1906c
eitangold <75008589+eitangold@users.noreply.github.com>
2023-06-18 09:53:58 +0300

    Added-lastview component

---
+++ src/components/LastVisited.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id='lastvisited'>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         
+      }
+   },
+   methods: {
+      async getLastVisited(){
+         
+      }
+   },
+   props: {
+      data:{
+         type: Object,
+         required: true,
+         
+      },
+   },
+
+}
+</script>
+
+<style>
+
+</style>
