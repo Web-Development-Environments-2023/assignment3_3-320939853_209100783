a145f90077ee87876273f27a8415766a3ae74424
eitangold <75008589+eitangold@users.noreply.github.com>
2023-07-22 21:10:10 +0300

    about page added

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="aboutpage">
+   <b-container id="container">
+
+   </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+
+</style>
